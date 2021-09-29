7dd1a8505d19caad19d116048fd0a8724bbbcfad
AlexanderD25 <91078529+25Alexander@users.noreply.github.com>
2021-09-29 03:15:41 +0300

    Add new components;

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Project.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,21 @@ namespace Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<TodoModel> _todoData;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoData = new BindingList<TodoModel>() { 
+                new TodoModel(){Text = "text"},
+                new TodoModel(){Text = "I dont know"},
+
+            };
+            List.ItemsSource = _todoData;
         }
     }
 }
