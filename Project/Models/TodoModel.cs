47c9ba4e22651d62b145b8227c6e158508afb92b
AlexanderD25 <91078529+25Alexander@users.noreply.github.com>
2021-09-29 03:04:21 +0300

    Create a new class "TodoModel" and add inside new values ;

---
+++ Project/Models/TodoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    class TodoModel
+    {
+        private bool _isDone;
+        private string text;
+
+        public DateTime Date { get; set; } = DateTime.Now;
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        public string  Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+
+    }
+}
